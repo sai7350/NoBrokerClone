15f330db58a7bcda72397fa3728dbf94aa6926fe
SaiShellikeri <saishellikeri@gmail.com>
2023-09-25 17:26:38 +0530

    header completed

---
+++ components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="grid grid-cols-1 sm:grid-cols-2 w-full shadow-lg py-4">
+        <div class="px-5 uppercase"> <span class="border- text-red-600">Find<span class="font-semibold">YourHouse</span> </span></div>
+        <div class="text-end px-5">
+            <span class="mx-3 px-2 pr-5 cursor-pointer font-sans">Sign up</span>
+            <span class="mx-3 p-2  cursor-pointer">Log in</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <Header/>
+        <div class="md:mt-24 md:test"> Unlock Accurate Property Pricing on FINDYOUHOUSE</div>
+     
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

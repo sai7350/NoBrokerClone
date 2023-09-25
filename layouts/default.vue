aa7887094febddac36dc0b1455f1d1624bbb33de
rahulSutar001 <sutarrahul709@gmail.com>
2023-09-25 17:09:25 +0530

    header completed

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <Header />
+        <slot/>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <ResultPage/>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

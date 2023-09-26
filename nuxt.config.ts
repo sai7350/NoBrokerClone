21d5827616e15b5ac2ffdf48f6d0d6d43e303747
bhavsarshweta <bhavsarshweta146@gmail.com>
2023-09-26 16:11:06 +0530

    code merged

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  modules: ['@formkit/nuxt'],
+  devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
+  build: {
+    transpile: ["vuetify"],
+  },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+ 
+
+ 
+});

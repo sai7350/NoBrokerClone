ad188fc19aedfd350d11554c5c2948aba353977c
bhavsarshweta <bhavsarshweta146@gmail.com>
2023-10-09 14:32:48 +0530

    scraping code added

--- scrap.js
+++ scrap.js
@@ -1,24 +1,91 @@
-const cheerio = require('cheerio');
-const axios = require('axios');
+const puppeteer = require("puppeteer");
 
-// Send an HTTP request to the website
-const url = 'https://www.nobroker.in/property/sale/pune/Baner?searchParam=W3sibGF0IjoxOC41NjQyNDUyLCJsb24iOjczLjc3Njg1MTEsInBsYWNlSWQiOiJDaElKeTlOZDhNLS13anNSZmF0Xy01Y1NrYUUiLCJwbGFjZU5hbWUiOiJCYW5lciJ9XQ==&radius=2.0&city=pune&locality=Baner';
-axios.get(url)
-  .then((response) => {
-    // Load the HTML content into Cheerio
-    const $ = cheerio.load(response.data);
+async function parseNoBrokerSwargate() {
+  // Launch the browser
+  const browser = await puppeteer.launch();
 
-    // Find an element by its ID
-    const elementWithId = $('#minDeposit');
+  // Open a new tab
+  const page = await browser.newPage();
 
-    // Extract data from the element
-    if (elementWithId.length) {
-      const data = elementWithId.text();
-      console.log(data);
-    } else {
-      console.log('Element with ID not found');
-    }
-  })
-  .catch((error) => {
-    console.error('Error fetching data:', error);
+  // URL for Swargate in Pune
+  const swargateURL =
+    "https://www.nobroker.in/property/sale/pune/Baner?searchParam=W3sibGF0IjoxOC41NjQyNDUyLCJsb24iOjczLjc3Njg1MTEsInBsYWNlSWQiOiJDaElKeTlOZDhNLS13anNSZmF0Xy01Y1NrYUUiLCJwbGFjZU5hbWUiOiJCYW5lciIsInNob3dNYXAiOmZhbHNlfV0=&radius=2.0&city=pune&locality=Katraj&isMetro=false";
+
+  // Visit the Swargate page and wait until network connections are completed
+  await page.goto(swargateURL, { waitUntil: "networkidle2" });
+
+  // Interact with the DOM to retrieve the titles
+  const titles = await page.evaluate(() => {
+    // Select all elements with crayons-tag class
+    return [
+      ...document.querySelectorAll(
+        ".infinite-scroll-component .nb__7nqQI .border-cardbordercolor #minDeposit"
+      ),
+    ].map((el) => el.textContent.trim());
   });
+
+  // Don't forget to close the browser instance to clean up the memory
+  await browser.close();
+  const extractedPrices = titles.map((text) => {
+    const matchLacs = text.match(/₹([\d.]+) Lacs/);
+    const matchCrores = text.match(/₹([\d.]+) Crores/);
+
+    if (matchLacs) {
+      return matchLacs[1] + " Lacs";
+    } else if (matchCrores) {
+      return matchCrores[1] + " Crores";
+    } else {
+      return null;
+    }
+  });
+
+  console.log(extractedPrices);
+
+  function convertLakhOrCroreStringToNumber(valueString) {
+    // Remove any non-numeric characters and convert to a number
+    const numericValue = parseFloat(valueString.replace(/[^\d.]/g, ""));
+
+    // Check if the string contains 'L' or 'cr' and perform the conversion
+    if (valueString.includes("Lacs")) {
+      return numericValue * 100000; // 1 Lakh = 100,000
+    } else if (valueString.includes("Crores")) {
+      return numericValue * 10000000; // 1 Crore = 10,000,000
+    } else {
+      return numericValue; // No 'L' or 'cr', so it's already a number
+    }
+  }
+
+  function formatPriceWithUnits(price) {
+    if (price >= 10000000) {
+      // If the price is 1 crore or more, display it in crores
+      return `${(price / 10000000).toFixed(2)} crores`;
+    } else if (price >= 100000) {
+      // If the price is 1 lakh or more, display it in lakhs
+      return `${(price / 100000).toFixed(2)} lakhs`;
+    } else {
+      // Otherwise, display it in raw numbers
+      return price.toString();
+    }
+  }
+
+  // Convert the array values to numeric prices
+  const numericPrices = extractedPrices.map(convertLakhOrCroreStringToNumber);
+
+  // Calculate the average price
+  const averagePrice =
+    numericPrices.reduce((sum, price) => sum + price, 0) / numericPrices.length;
+  // Calculate the maximum price
+  const maxPrice = Math.max(...numericPrices);
+
+  // Calculate the minimum price
+  const minPrice = Math.min(...numericPrices);  
+
+  const AveragePrice = formatPriceWithUnits(averagePrice);
+  const MaxPrice = formatPriceWithUnits(maxPrice);
+  const MinPrice = formatPriceWithUnits(minPrice);
+
+  console.log("Average"+AveragePrice);
+  console.log("maximum"+MaxPrice+"minimum"+MinPrice);
+}
+
+parseNoBrokerSwargate();
